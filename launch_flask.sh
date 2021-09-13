6626042d1ff3ed3e062d8351446cbad85bbbd4ff
Olasimbo <olahsymbo@gmail.com>
2021-09-13 16:11:37 +0100

    nc

--- launch_flask.sh
+++ launch_flask.sh
@@ -1,5 +1,6 @@
 cd src &
-virtualenv -p /usr/local/opt/python@3.7/bin/python3.7 .virtualenv &
+#virtualenv -p /usr/local/opt/python@3.7/bin/python3.7 .virtualenv &
+virtualenv -p /usr/bin/python3.7 .virtualenv &
 pip install -r src/requirements.txt &
 source .virtualenv/bin/activate &
 gunicorn --bind 0.0.0.0:8080 wsgi:app --workers 8 --threads 4
